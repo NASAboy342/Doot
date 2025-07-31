b2490fe1b12d688e154efea246e925bfd34209a3
PinSopheaktra <sopheakpin342@gmail.com>
2025-07-31 23:33:16 +0700

    add operation properties

---
+++ Doot/Doot/Enums/EnumOperationType.cs
@@ -0,0 +1,6 @@
+namespace Doot.Enums;
+
+public enum EnumOperationType
+{
+    
+}
--- Doot/Doot/Models/Operation.cs
+++ Doot/Doot/Models/Operation.cs
@@ -1,6 +1,10 @@
+using Doot.Enums;
+
 namespace Doot.Models;
 
 public class Operation
 {
-    
+    public EnumOperationType Type { get; set; }
+    public string PlaceHolder { get; set; } = "";
+    public Dictionary<string, string> Results { get; set; } = new Dictionary<string, string>();
 }
